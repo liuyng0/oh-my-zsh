5a63fdad00483ecab3c8de36a5ec82c8594fa561
chophi <chophi@foxmail.com>
2020-05-19 13:07:41 +0800

    don't source the conda if zeppelin exists

--- start.zsh
+++ start.zsh
@@ -102,18 +102,22 @@ source $ZSH/oh-my-zsh.sh
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$($HOME/opt/anaconda3/bin/conda 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "$HOME/opt/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "$HOME/opt/anaconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="$HOME/opt/anaconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
 
+POSSIBLE_ZEPPELIN_DAEMON=`ls $HOME/Software/zeppelin*/bin/zeppelin-daemon.sh 2>/dev/null`
+if [[ "$POSSIBLE_ZEPPELIN_DAEMON" == "" ]] || ! [ -e  $POSSIBLE_ZEPPELIN_DAEMON ]; then
+
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    __conda_setup="$($HOME/opt/anaconda3/bin/conda 'shell.zsh' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "$HOME/opt/anaconda3/etc/profile.d/conda.sh" ]; then
+            . "$HOME/opt/anaconda3/etc/profile.d/conda.sh"
+        else
+            export PATH="$HOME/opt/anaconda3/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+    # <<< conda initialize <<<
+fi

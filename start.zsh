8256e881aec870ced1269ce465252f0a658414d8
chophi <chophi@foxmail.com>
2023-06-15 22:28:26 +0800

    use jenv

--- start.zsh
+++ start.zsh
@@ -141,3 +141,25 @@ command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 
 [[ -s "$HOME/.gitconfig" ]] && export DOOMGITCONFIG="$HOME/.gitconfig"
+eval export PATH="/Users/chophi/.jenv/shims:${PATH}"
+export JENV_SHELL=zsh
+export JENV_LOADED=1
+unset JAVA_HOME
+unset JDK_HOME
+source '/opt/homebrew/Cellar/jenv/0.5.6/libexec/libexec/../completions/jenv.zsh'
+jenv rehash 2>/dev/null
+jenv refresh-plugins
+jenv() {
+  type typeset &> /dev/null && typeset command
+  command="$1"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  enable-plugin|rehash|shell|shell-options)
+    eval `jenv "sh-$command" "$@"`;;
+  *)
+    command jenv "$command" "$@";;
+  esac
+}
